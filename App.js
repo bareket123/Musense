6ae9a51f65bd8de0f6736d4884e4227b40c9f5a2
barek <bareketatia@gmail.com>
2023-04-05 17:37:06 +0300

    add cool buttons

--- App.js
+++ App.js
@@ -5,20 +5,29 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
-import DetailsScreen from './screens/DetailsScreen';
 import PopularNow from './screens/PopularNow';
 import PlayedRecently from './screens/PlayedRecently';
 import MusicByArist from './screens/MusicByArtist';
 import MusicByFriends from './screens/MusicByFriends';
 import MyPlaylist from './screens/MyPlaylist';
+import { Ionicons } from '@expo/vector-icons';
+
 
 export default function App() {
   const Stack = createStackNavigator();
   return (
      <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{
+          headerTitle: () => (
+              <>
+                <Ionicons name="ios-home" size={24} color="black" />
+
+              </>
+          ),
+          headerTitleAlign: 'center',
+        }}
+        />
         <Stack.Screen name='Popular' component={PopularNow}/>
         <Stack.Screen name='played' component={PlayedRecently}/>
         <Stack.Screen name='artist' component={MusicByArist}/>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,42 +1,63 @@
+
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { SafeAreaView, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-export default function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' ,flexDirection: 'column'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-        <Button
-            title="Go to popular"
-            onPress={() => navigation.navigate('Popular')}
+const HomeScreen = ({ navigation }) => {
+    return (
+        <SafeAreaView style={styles.container}>
 
-        />
-        <Button
-            title="played recently"
-            onPress={() => navigation.navigate('played')}
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('Popular')}
+            >
+                <Text style={styles.buttonText}>Go to Popular</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('played')}
+            >
+                <Text style={styles.buttonText}>Played Recently</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('artist')}
+            >
+                <Text style={styles.buttonText}>Played Music by Artist</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('friends')}
+            >
+                <Text style={styles.buttonText}>Played Music by Friends</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('playlist')}
+            >
+                <Text style={styles.buttonText}>My Playlist</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
+};
 
-        />
-             <Button
-            title="played Music by artist"
-            onPress={() => navigation.navigate('artist')}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+        backgroundColor: '#2196F3',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 10,
+        marginHorizontal: 5,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
 
-        />
-           
-             <Button
-            title="played Music by friends"
-            onPress={() => navigation.navigate('friends')}
-
-        />
-              <Button
-            title="my playlist"
-            onPress={() => navigation.navigate('playlist')}
-
-        />
-        
-         
-    </View>
-  );
-}
+export default HomeScreen;
