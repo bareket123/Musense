cbc289985e359ba3864772ce01dd5ffb803a93da
אופיר יהודה <ofir2738@gmail.com>
2023-08-18 14:28:12 +0300

    Fix my playlist page + can remove a song-ofir

--- screens/MyPlaylist.js
+++ screens/MyPlaylist.js
@@ -1,70 +1,210 @@
-import React, {useState} from 'react';
-import {ScrollView, View, Text, Button, StyleSheet, Image} from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import { useRoute } from '@react-navigation/native';
-import { useSelector, useDispatch } from 'react-redux';
+// import React, { useState, useEffect } from 'react';
+// import {View, Text, FlatList, StyleSheet, TouchableOpacity, Button} from 'react-native';
+// import { MaterialCommunityIcons } from '@expo/vector-icons';
+// import { useSelector } from 'react-redux';
+// import { Audio } from 'expo-av';
+//
+// export default function MyPlaylist({ navigation }) {
+//     const [sound, setSound] = useState(null);
+//     const { playList, playedRecently } = useSelector(state => state.reducer);
+//
+//     const playSound = async (song) => {
+//         if (!song) {
+//             return;
+//         }
+//
+//         try {
+//             if (sound) {
+//                 await sound.unloadAsync();
+//             }
+//
+//             const { sound: newSound } = await Audio.Sound.createAsync({ uri: song.url });
+//             setSound(newSound);
+//             await newSound.playAsync();
+//         } catch (error) {
+//             console.error('Error playing sound:', error);
+//         }
+//     };
+//
+//     const pauseSound = () => {
+//         if (sound) {
+//             sound.pauseAsync().catch(error => {
+//                 console.error('Error pausing sound:', error);
+//             });
+//         }
+//     };
+//
+//     useEffect(() => {
+//         return () => {
+//             if (sound) {
+//                 sound.unloadAsync().catch(error => {
+//                     console.error('Error unloading sound:', error);
+//                 });
+//             }
+//         };
+//     }, [sound]);
+//
+//     const renderSong = ({ item }) => (
+//         <View style={{ padding: 20, flexDirection: 'row', alignItems: 'center', left: '20%' , backgroundColor:'pink'}}>
+//             <MaterialCommunityIcons name="play-box" size={24} color="black" />
+//             <Text style={{ fontSize: 16, color: 'purple' }}>{item.title}</Text>
+//             <TouchableOpacity style={{ marginLeft: 50 }} onPress={() => playSound(item)}>
+//                 <Text>Play</Text>
+//             </TouchableOpacity>
+//             <TouchableOpacity style={{ marginLeft: 50 }} onPress={() => pauseSound()}>
+//                 <Text>Pause</Text>
+//             </TouchableOpacity>
+//
+//         </View>
+//     );
+//
+//     return (
+//         <View>
+//             <Text>My Playlist:</Text>
+//             <FlatList
+//                 data={playList}
+//                 renderItem={renderSong}
+//                 keyExtractor={(item) => item.songIndex}
+//             />
+//         </View>
+//     );
+// }
+//
+//
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         padding: 10,
+//         backgroundColor:'pink',
+//     },
+//     text: {
+//         fontSize: 16,
+//         lineHeight: 24,
+//     },
+// });
 
 
-export default function MyPlaylist ({ navigation }) {
-    const [image, setImage] = useState(null);
-    const {playList ,playedRecently} = useSelector(state => state.reducer)
-    // const route = useRoute();
-    // const receivedArray = route.params?.data || [];
 
-    const pickImage = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            aspect: [4, 3],
-            quality: 1,
-        });
 
-        if (!result.canceled) {
-            setImage(result.uri);
+
+
+
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { Audio } from 'expo-av';
+import { setPlaylist } from '../redux/actions';
+
+export default function MyPlaylist({ navigation }) {
+    const [sound, setSound] = useState(null);
+    const { playList, playedRecently } = useSelector(state => state.reducer);
+    const dispatch = useDispatch();
+
+    const [songs, setSongs] = useState(playList.map(song => ({ ...song, isFavorite: false })));
+
+    const playSound = async (song) => {
+        if (!song) {
+            return;
+        }
+
+        try {
+            if (sound) {
+                await sound.unloadAsync();
+            }
+
+            const { sound: newSound } = await Audio.Sound.createAsync({ uri: song.url });
+            setSound(newSound);
+            await newSound.playAsync();
+        } catch (error) {
+            console.error('Error playing sound:', error);
         }
     };
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>choose an Atist:</Text>
-            <Button title="Upload Picture" onPress={pickImage} />
-            {typeof image === 'string' && (
-                <Image
-                    source={{ uri: image }}
-                    style={{ width: 200, height: 200, marginTop: 20 }}
+
+    const pauseSound = () => {
+        if (sound) {
+            sound.pauseAsync().catch(error => {
+                console.error('Error pausing sound:', error);
+            });
+        }
+    };
+
+    useEffect(() => {
+        return () => {
+            if (sound) {
+                sound.unloadAsync().catch(error => {
+                    console.error('Error unloading sound:', error);
+                });
+            }
+        };
+    }, [sound]);
+
+    function toggleFavorite(index) {
+        const updatedSongs = songs.map((song, i) => {
+            if (i === index) {
+                return { ...song, isFavorite: !song.isFavorite };
+            }
+            return song;
+        });
+        setSongs(updatedSongs);
+    }
+
+    function removeSong(index) {
+        const updatedSongs = songs.filter((_, i) => i !== index);
+        setSongs(updatedSongs);
+    }
+
+    const renderSong = ({ item, index }) => (
+        <View style={{ padding: 20, flexDirection: 'row', alignItems: 'center', left: '20%', backgroundColor: 'pink' }}>
+            <MaterialCommunityIcons name="play-box" size={24} color="black" />
+            <Text style={{ fontSize: 16, color: 'purple' }}>{item.title}</Text>
+            <TouchableOpacity style={{ marginLeft: 50 }} onPress={() => playSound(item)}>
+                <Text>Play</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ marginLeft: 50 }} onPress={() => pauseSound()}>
+                <Text>Pause</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => {
+                    toggleFavorite(index);
+                    removeSong(index);
+                }}
+            >
+                <MaterialCommunityIcons
+                    // name="heart"
+                    // size={30}
+                    // color={item.isFavorite ? 'green' : 'red'}
+                    name="delete"
+                    size={30}
+                    color="black"
                 />
-            )}
-
-            <Button
-                title="Go back to Home"
-                onPress={() => navigation.navigate("Home")}
-            />
-            <View>
-                <Text>Received Array:</Text>
-                {
-                    playList.map((item, index) => {
-                        return(
-                            <Text>{item.title}</Text>
-                        )
-                    })}
-            </View>
+            </TouchableOpacity>
         </View>
-
     );
 
+    return (
+        <View>
+            <Text>My Playlist:</Text>
+            <FlatList
+                data={songs}
+                renderItem={renderSong}
+                keyExtractor={(item, index) => index.toString()}
+            />
+        </View>
+    );
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 10,
+        backgroundColor: 'pink',
     },
     text: {
         fontSize: 16,
         lineHeight: 24,
     },
 });
-
-
-
 
 
 
--- screens/PopularNow.js
+++ screens/PopularNow.js
@@ -3,30 +3,25 @@ import {View, Text, TouchableOpacity, StyleSheet, Button, ScrollView, SafeAreaVi
 import { Audio } from 'expo-av';
 import {AntDesign} from "@expo/vector-icons";
 import { useSelector, useDispatch } from 'react-redux';
-import {setPlayedRecently, setPlaylist} from "../redux/actions"; // Import useSelector and useDispatch from react-redux
-// import { addToPlayedRecently } from '../actions/actions'; // Import your action creator
-
-
+import {setPlayedRecently, setPlaylist} from "../redux/actions";
+// import { addToPlayedRecently } from '../actions/actions';
 
 
 const PopularNow = () => {
-
     // const {playList ,playedRecently} = useSelector(state => state.reducer)
-
     // const {setMyPlaylist}=useContext(GlobalStateContext);
     // const [playedRecently,setPlayedRecently]=useState([])
-    // const playedRecently = useSelector((state) => state.cardData); // Access the playedRecently state from redux
+    // const playedRecently = useSelector((state) => state.cardData);
     const dispatch = useDispatch(); // Get the dispatch function
     const [sound, setSound] = useState();
-    const [songsArray, setSongsArray] = useState([]); // Initialize songsArray as a state
+    const [songsArray, setSongsArray] = useState([]);
 
     async function playSound(index) {
         const song = songsArray[index];
-        dispatch(setPlayedRecently(song)); // Dispatch the action to add the song to playedRecently state in redux
+        dispatch(setPlayedRecently(song));
         if (!song) {
-            return; // Handle invalid index
+            return;
         }
-
         const { sound } = await Audio.Sound.createAsync({ uri: song.url });
         setSound(sound);
         await sound.playAsync();
@@ -46,28 +41,20 @@ const PopularNow = () => {
         };
     }, [sound]);
 
-    // Assume you fetch songs and update the songsArray here
     useEffect(() => {
-        // Fetch songs from API and update the songsArray state
-        const fetchedSongs = []; // Fetch songs from API
+        const fetchedSongs = [];
         setSongsArray(fetchedSongs);
     }, []);
     // const [sound, setSound] = useState();
     // const songsArray=[];
-    //
-    //
     // let urlAudio='';
-    //
     // async function playSound() {
-    //
     //     const { sound } = await Audio.Sound.createAsync(
     //         { uri: urlAudio }
     //     );
     //     setSound(sound);
-    //
     //     await sound.playAsync(); // Play the audio
     // }
-    //
     // React.useEffect(() => {
     //     return sound
     //         ? () => {
@@ -76,7 +63,7 @@ const PopularNow = () => {
     //         : undefined;
     // }, [sound]);
     //
-    //
+
     useEffect(() => {
         fetchPlaylist();
     }, []);
@@ -90,17 +77,12 @@ const PopularNow = () => {
                 artist: song.subtitle,
                 url: song.hub.actions[1].uri,
                 coverImage: song.images.background,
-                isFavorite: false // Initialize as not favorite//////////////////////////////////
-
+                isFavorite: false ////////////////////////////////////
             };
-
             tempArray.push(currentSong);
         });
-
         setSongsArray(tempArray)
-
     }
-
 
     // const fetchSingleUrl =async()=>{
     //     const options = {
@@ -125,7 +107,7 @@ const PopularNow = () => {
             }
         };
 
-        fetch('https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=20&startFrom=0', options)
+        fetch('https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=10&startFrom=0', options)
             .then(response => response.json())
             .then(response => getSong(response))
             .catch(err => console.error(err));
@@ -163,87 +145,16 @@ const PopularNow = () => {
         </View>
     );
 
-
-
     return (
-        // <ScrollView style={styles.container}>
-        //     <SafeAreaView style={styles.SafeAreaView} >
         <View style={styles.container}>
             <View>
-
                 <FlatList
                     keyExtractor={(item)=>item.songIndex}
                     data={songsArray}
                     renderItem={renderSong}
-
                 />
-                {/*{*/}
-                {/*    playedRecently.length>0&&*/}
-                {/*    playedRecently.map((song)=>{*/}
-                {/*        console.log(song.title+"*****\n")*/}
-                {/*    })*/}
-                {/*}*/}
-                {/*<FlatList*/}
-                {/*    data={songsArray}*/}
-                {/*    renderItem={renderSong}*/}
-                {/*    keyExtractor={(item) => item.songIndex}*/}
-
-                {/*/>*/}
             </View>
-
-            {/*{songsArray.map((song, index) => (*/}
-            {/*    */}
-            {/*     <TouchableOpacity*/}
-            {/*        onPress={() => togglePlaying}*/}
-            {/*        style={{*/}
-            {/*            width: 80,*/}
-            {/*            height: 80,*/}
-            {/*            borderRadius: 40,*/}
-            {/*            // backgroundColor: 'khaki',*/}
-            {/*            justifyContent: 'center',*/}
-            {/*            alignItems: 'center',*/}
-            {/*            marginRight: 20,*/}
-            {/*            marginLeft: 20,*/}
-            {/*        }}>*/}
-
-            {/*          <Text> song name:{song.title}</Text>*/}
-            {/*        {*/}
-            {/*            playing?*/}
-
-
-            {/*                <AntDesign onPress={()=>{togglePlaying(),playSound(index)}}  name="play" size={60} color="white"   />*/}
-
-            {/*                :*/}
-            {/*                <AntDesign onPress={()=>{togglePlaying(),pauseSound(index)}} name="pausecircle" size={60} color="white" />*/}
-
-
-            {/*        }*/}
-
-            {/*    </TouchableOpacity>*/}
-            {/*    <Button*/}
-            {/*        key={index}*/}
-            {/*        title={`Play ${song.title}`}*/}
-            {/*        onPress={() => playSound(index)}*/}
-            {/*    />*/}
-            {/*))}*/}
-
-
-            {/*{*/}
-            {/*    songsArray.map((song, index) => (*/}
-            {/*    <Button*/}
-            {/*        key={index}*/}
-            {/*        title={`Play ${song.title}`}*/}
-            {/*        onPress={() => playSound(index)}*/}
-            {/*    />*/}
-            {/*))}*/}
-            {/*/!* ... other components *!/*/}
-            {/*<TouchableOpacity style={styles.playButton} onPress={playSound}>*/}
-            {/*    <Text style={styles.playButtonText}>Play Song </Text>*/}
-            {/*    <Button title={"print"} onPress={printArray}/>*/}
-            {/*</TouchableOpacity>*/}
         </View>
-        //     </SafeAreaView>
-        // </ScrollView>
     );
 };
 
@@ -269,6 +180,161 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     }
 });
-
 export default PopularNow;
+
+
+
+
+
+// import React, {useEffect, useState} from 'react';
+// import {View, Text, TouchableOpacity, StyleSheet, Button, ScrollView, SafeAreaView, FlatList} from 'react-native';
+// import { Audio } from 'expo-av';
+// import {AntDesign} from "@expo/vector-icons";
+// import { useSelector, useDispatch } from 'react-redux';
+// import {setPlayedRecently, setPlaylist} from "../redux/actions";
+//
+
+// const PopularNow = () => {
+//     const dispatch = useDispatch();
+//     const [sound, setSound] = useState();
+//     const [songsArray, setSongsArray] = useState([]);
+//     const [isInMyPlaylist, setIsInMyPlaylist] = useState(false);
+//
+//     async function playSound(index) {
+//         const song = songsArray[index];
+//         dispatch(setPlayedRecently(song));
+//         if (!song) {
+//             return;
+//         }
+//
+//         const { sound } = await Audio.Sound.createAsync({ uri: song.url });
+//         setSound(sound);
+//         await sound.playAsync();
+//     }
+//
+//     async function pauseSound(index) {
+//         const song = songsArray[index];
+//         if (sound) {
+//             await sound.pauseAsync();
+//         }
+//     }
+//     useEffect(() => {
+//         return () => {
+//             if (sound) {
+//                 sound.unloadAsync();
+//             }
+//         };
+//     }, [sound]);
+//
+//     useEffect(() => {
+//         const fetchedSongs = [];
+//         setSongsArray(fetchedSongs);
+//     }, []);
+//
+//     useEffect(() => {
+//         fetchPlaylist();
+//     }, []);
+//
+//     function getSong(response) {
+//         let tempArray=[]
+//         response.tracks.map((song,index) => {
+//             const currentSong = {
+//                 songIndex:index,
+//                 title: song.title,
+//                 artist: song.subtitle,
+//                 url: song.hub.actions[1].uri,
+//                 coverImage: song.images.background,
+//             };
+//             tempArray.push(currentSong);
+//         });
+//         setSongsArray(tempArray)
+//     }
+//
+//     const fetchPlaylist=async ()=>{
+//         const options = {
+//             method: 'GET',
+//             headers: {
+//                 'X-RapidAPI-Key': '77f4e88fbcmsh34c35cf21256c6ap1326abjsn36b18c917e58',
+//                 'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+//             }
+//         };
+//         fetch('https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=20&startFrom=0', options)
+//             .then(response => response.json())
+//             .then(response => getSong(response))
+//             .catch(err => console.error(err));
+//     }
+//
+//     function handleAddToFavorites(index) {
+//         const song = songsArray[index];
+//         dispatch(setPlaylist(song));
+//     }
+//
+//     function handleRemoveFromFavorites(index) {
+//         const song = songsArray[index];
+//         const updatedSongsArray = songsArray.filter((_, i) => i !== index);
+//         setSongsArray(updatedSongsArray);
+//     }
+//
+//     function handleToggleFavorite(index) {
+//         const song = songsArray[index];
+//         if (isInMyPlaylist) {
+//             handleRemoveFromFavorites(index);
+//         } else {
+//             handleAddToFavorites(index);
+//         }
+//         setIsInMyPlaylist(!isInMyPlaylist);
+//     }
+//
+//     const renderSong = ({ item }) => (
+//         <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10 }}>
+//             <View style={{ flex: 1 }}>
+//                 <Text style={{ fontSize: 15, color: 'black' }} >{item.title}</Text>
+//             </View>
+//             <TouchableOpacity style={{ flexDirection: 'row' }}>
+//                 <AntDesign onPress={() => pauseSound(item.songIndex).then(r => {})} name={"pausecircle"} size={30} color="black" />
+//                 <AntDesign onPress={() => playSound(item.songIndex).then(r => {})} name="play" size={30} color="black" />
+//                 <TouchableOpacity onPress={() => handleToggleFavorite(item.songIndex)}>
+//                     <AntDesign name="heart" size={30} color={isInMyPlaylist ? 'red' : 'green'} />
+//                 </TouchableOpacity>
+//             </TouchableOpacity>
+//         </View>
+//     );
+//
+//     return (
+//         <View style={styles.container}>
+//             <View>
+//                 <FlatList
+//                     keyExtractor={(item)=>item.songIndex}
+//                     data={songsArray}
+//                     renderItem={renderSong}
+//                 />
+//             </View>
+//         </View>
+//     );
+// };
+//
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor:'pink'
+//     },
+//     playButton: {
+//         backgroundColor: 'green',
+//         padding: 10,
+//         borderRadius: 8,
+//         marginTop: 20,
+//     },
+//     playButtonText: {
+//         color: 'white',
+//         fontSize: 16,
+//         fontWeight: 'bold',
+//     },
+//
+//     SafeAreaView:{
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//     }
+// });
+//
+// export default PopularNow;
 
