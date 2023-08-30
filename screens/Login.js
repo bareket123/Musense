d4bc50a804ed377a3b47c8e24f695c44774fe09f
Hanny <hannylevy63@gmail.com>
2023-08-31 01:12:51 +0300

    adding picture of user login

--- screens/Login.js
+++ screens/Login.js
@@ -1,15 +1,16 @@
 import React, {useState} from 'react';
-import {ImageBackground, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {ImageBackground, ScrollView, StyleSheet, Text, View,Image} from 'react-native';
 import axios from "axios";
 import {Button, RadioButton, TextInput} from 'react-native-paper';
 import isEmail from 'validator/lib/isEmail';
 import {AntDesign, MaterialCommunityIcons, MaterialIcons} from '@expo/vector-icons';
-import image from '../images/musicBackGround.jpg';
+import pic from '../images/musicBackGround.jpg';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {LOCAL_SERVER_URL, setToken, setUsername,setPicture} from "../redux/actions";
 import {useDispatch, useSelector} from "react-redux";
 import ErrorAlert from "./ErrorAlert";
 import {LOGIN_SUCCESSFULLY, SIGN_UP_SUCCESSFULLY} from "./Constans";
+import * as ImagePicker from 'expo-image-picker';
 
 
 export default function Login () {
@@ -17,63 +18,105 @@ export default function Login () {
 
     const [usernameInput, setUsernameInput] = useState("");
     const [password, setPassword] = useState("");
-    const [email,setEmail]=useState("");
-    const {picture}=useSelector(state => state.reducer);
+    const [email, setEmail] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [checked, setChecked] = useState('');
-    const {isLoggedIn}= useSelector(state => state.reducer);
-    const[messageCode, setMessageCode] = useState(0);
+    const {isLoggedIn} = useSelector(state => state.reducer);
+    const [messageCode, setMessageCode] = useState(0);
+
+    const [uploadPic, setUploadPic] = useState(null);
 
 
-
-    async function handleButtonPressed() {
-
-        let res;
-        try {
-            if (checked==='signUp'){
-                console.log("enter sign")
-                res = await axios.create({baseURL: LOCAL_SERVER_URL}).post('/sign-up?username=' + usernameInput + '&password=' + password+'&email='+email+"&picture="+picture)
-                if (res.data.success) {
-                    setMessageCode(SIGN_UP_SUCCESSFULLY)
-
-                    setConfirmPassword("");
-                    setEmail("");
-                    setChecked('login');
-
-                }else {
-                    setMessageCode(res.data.errorCode);
-                }
-
-            }else if (checked==='login') {
-                console.log("enter login")
-
-                res = await axios.create({baseURL: LOCAL_SERVER_URL}).post('/login?username=' + usernameInput + '&password=' + password)
-                console.log(res.data)
-                if (res.data.success){
-                    const token=res.data.token;
-                    await AsyncStorage.setItem('token', token);
-                    await AsyncStorage.setItem('username', usernameInput);
-                    await setUserPic(token)
-                    dispatch(setToken(token));
-                    dispatch(setUsername(usernameInput))
-                    console.log("is logged in :" + isLoggedIn)
-                    setMessageCode(LOGIN_SUCCESSFULLY)
-                }else {
-                    setMessageCode(res.data.errorCode)
-
-                }
-
-            }
-
-        } catch (error) {
-            console.log(error)
+    const selectImage = async () => {
+        let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (permissionResult.granted === false) {
+            alert("Permission to access camera roll is required!");
+            return;
         }
-        setUsernameInput("")
-        setPassword("")
-        setMessageCode(0);
+        let result = await ImagePicker.launchImageLibraryAsync();
+        if (!result.canceled) {
+            setUploadPic(result.assets[0]);
+        }
+    };
 
+        const uploadImage = async () => {
+        if (uploadPic) {
+            const formData = new FormData();
+            formData.append('image', {
+                uri: uploadPic.uri,
+                type: 'image/jpeg',
+                name: 'image.jpg',
+            });
 
+            try {
+                const response = await axios.post(LOCAL_SERVER_URL + '/upload', formData, {
+                    params: {
+                        username: usernameInput,
+                    },
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                });
+
+                return { success: true, imageUrl: response.data.imageUrl }; // Return the upload success and imageUrl
+            } catch (error) {
+                return { success: false, errorCode: response.data.errorCode };
+            }
+        }
+    };
+    async function handleButtonPressed() {
+        try {
+            if (checked === 'signUp') {
+                console.log("enter sign");
+
+                const uploadResponse = await uploadImage(); // Wait for the image upload
+                console.log(uploadResponse);
+
+                if (uploadResponse.success) {
+                  const  res = await axios.create({baseURL: LOCAL_SERVER_URL}).post('/sign-up?username=' + usernameInput + '&password=' + password+'&email='+email+'&imageUrl='+uploadResponse.imageUrl)
+
+                    if (res.data.success) {
+                        alert(SIGN_UP_SUCCESSFULLY);
+                        setConfirmPassword("");
+                        setEmail("");
+                        setChecked('login');
+                        setUploadPic(null);
+                    } else {
+                        setMessageCode(res.data.errorCode);
+                    }
+                } else {
+                    alert(uploadResponse.errorCode);
+                }
+            } else if (checked === 'login') {
+                console.log("enter login")
+                let res;
+                res = await axios.create({baseURL: LOCAL_SERVER_URL}).post('/login?username=' + usernameInput + '&password=' + password)
+                            console.log(res.data)
+                            if (res.data.success){
+                                const token=res.data.token;
+                                await AsyncStorage.setItem('token', token);
+                                await AsyncStorage.setItem('username', usernameInput);
+                                dispatch(setToken(token));
+                                dispatch(setUsername(usernameInput))
+                                await setUserPic(token);
+                                console.log("is logged in :" + isLoggedIn)
+                                alert(LOGIN_SUCCESSFULLY);
+                            }else {
+                                setMessageCode(res.data.errorCode)
+
+                            }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+        setUsernameInput("");
+        setPassword("");
     }
+
+
+
+
     function emailValidation(userEmail){
         let valid;
         valid = !!isEmail(userEmail);
@@ -105,24 +148,10 @@ export default function Login () {
      await AsyncStorage.setItem('picture', picUrl);
      dispatch(setPicture(picUrl))
 
-
     }
 
-    // const pickImage = async () => {
-    //     let result = await ImagePicker.launchImageLibraryAsync({
-    //         mediaTypes: ImagePicker.MediaTypeOptions.All,
-    //         allowsEditing: true,
-    //         aspect: [4, 3],
-    //         quality: 1,
-    //     });
-    //
-    //     if (!result.canceled) {
-    //         setPicture(result.uri);
-    //         console.log(picture)
-    //     }
-    // };
     return (
-        <ImageBackground source={image} style={styles.background} >
+        <ImageBackground source={pic} style={styles.background} >
             <ScrollView style={styles.container}>
                 <View>
 
@@ -189,18 +218,20 @@ export default function Login () {
                                         />
 
                                     </View>
-                                    {/*<View style={[styles.viewStyle,{marginBottom:20}]}>*/}
-                                    {/*    <MaterialIcons name="image" size={24} color="black" />*/}
-                                    {/*    <TextInput*/}
-                                    {/*        placeholder="add link to picture"*/}
-                                    {/*        value={picture}*/}
-                                    {/*        mode={"outlined"}*/}
-                                    {/*        onChangeText={setPicture}*/}
-                                    {/*        style={styles.textInput}*/}
-                                    {/*    />*/}
+                                    <View style={[styles.viewStyle]}>
+                                        <Button style={styles.button} labelStyle={{color:'white',fontSize:21,fontWeight: 'bold'}}
+                                                onPress={selectImage}
+                                        >Upload Picture </Button>
 
-                                    {/*</View>*/}
+                                    </View>
+                                    <View style={[styles.viewStyle]}>
+                                        {uploadPic && <Image source={{ uri: uploadPic.uri }} style={styles.image} />}
+                                    </View>
+                                    {/*<Button style={styles.button} labelStyle={{color:'white',fontSize:21,fontWeight: 'bold'}}*/}
+                                    {/*        onPress={uploadImage}*/}
+                                    {/*>Upload  </Button>*/}
                                 </View>
+
                             }
                             <View style={{ justifyContent: 'center', alignItems: 'center' }}>
                                 <Button style={styles.button} labelStyle={{color:'white',fontSize:21,fontWeight: 'bold'}}
@@ -284,8 +315,12 @@ const styles = StyleSheet.create({
         width:200,
         paddingHorizontal: 0,
         shadowColor: 'black',
-    }
-
+    },
+    image: {
+        width: 200,
+        height: 200,
+        marginTop: 20,
+    },
 
 
 });
