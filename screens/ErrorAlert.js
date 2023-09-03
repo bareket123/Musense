bf6735a2aca056c33d71adb6bc98cf8bdd034c38
אופיר יהודה <ofir2738@gmail.com>
2023-09-03 11:07:07 +0300

    design component

--- screens/ErrorAlert.js
+++ screens/ErrorAlert.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Alert } from 'react-native';
-import alertStyle from "./alertStyle";
+import alertStyle from "../styles/alertStyle";
 
 function ErrorAlert(props) {
     let messageForUser='';
@@ -36,6 +36,7 @@ function ErrorAlert(props) {
             case 1008:
                 messageForUser = 'ERROR PLAYLIST NOT EXIST';
                 break;
+
 
             case 1009:
                 messageForUser = 'ERROR_IN_ANSWERS_DETAILS';
@@ -83,3 +84,75 @@ function ErrorAlert(props) {
 }
 
 export default ErrorAlert;
+
+
+
+
+// import React, { useState } from 'react';
+// import { Alert } from 'react-native';
+// import alertStyle from "./alertStyle";
+//
+// function ErrorAlert(props) {
+//     let messageForUser='';
+//     const [visible, setVisible] = useState(true);
+//     let message = props.message;
+//     let type=message<1012? 'ERROR':'SUCCESS';
+//     if (visible){
+//         switch (message) {
+//             case 1000:
+//                 messageForUser = 'error missing username ';
+//                 break;
+//             case 1001:
+//                 messageForUser = 'error missing password';
+//                 break;
+//             case 1002:
+//                 messageForUser = "error weak password";
+//                 break;
+//             case 1003:
+//                 messageForUser = 'error username already exists ';
+//                 break;
+//             case 1004:
+//                 messageForUser = 'error wrong login details ';
+//                 break;
+//             case 1005:
+//                 messageForUser = 'error not found user';
+//                 break;
+//             case 1006:
+//                 messageForUser = 'error not found friend';
+//                 break;
+//             case 1007:
+//                 messageForUser = 'error wrong song details';
+//                 break;
+//             case 1008:
+//                 messageForUser = 'error playlist not exist';
+//                 break;
+//             case 1009:
+//                 messageForUser = 'error in answers details';
+//                 break;
+//             case 1010:
+//                 messageForUser = 'error wrong answers details';
+//                 break;
+//             case 1011:
+//                 messageForUser = 'error no such connection';
+//                 break;
+//             case 1012:
+//                 messageForUser = 'image upload failed';
+//                 break;
+//             ///////////////////////////////constant
+//             case 1013:
+//                 messageForUser = 'SignUp successfully';
+//                 break;
+//             case 1014:
+//                 messageForUser = 'LogIn successfully';
+//                 break;
+//             case 1015:
+//                 messageForUser = 'following';
+//                 break;
+//             case 1016:
+//                 messageForUser = 'delete';
+//                 break;
+//             case 1017:
+//                 messageForUser = 'something went wrong';
+//                 break;
+//         }
+//     }
