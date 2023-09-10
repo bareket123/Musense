c7f6b11ce43c1d997abbbb740eacbcad0ef7981e
אופיר יהודה <ofir2738@gmail.com>
2023-09-10 21:29:43 +0300

    fixing MyPlaylist.js design

--- screens/MyPlaylist.js
+++ screens/MyPlaylist.js
@@ -1,62 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import {View, Text, FlatList, StyleSheet, TouchableOpacity, Button} from 'react-native';
-import {AntDesign, MaterialCommunityIcons} from '@expo/vector-icons';
+import { View, Text, ImageBackground, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { useSelector } from 'react-redux';
-import { Audio } from 'expo-av';
 import axios from "axios";
 import Player from "./Player";
-import {LOCAL_SERVER_URL, setPlaylist} from "../redux/actions";
-import {useDispatch} from "react-redux";
+import { LOCAL_SERVER_URL } from "../redux/actions";
 import ErrorAlert from "./ErrorAlert";
-import Logo from "./Logo";
-import  myPlaylistStyle from '../styles/myPlaylistStyle'
-import globalStyles from "../styles/globalStyles";
+import myPlaylistStyle from '../styles/myPlaylistStyle';
 
 export default function MyPlaylist({ navigation }) {
-     const {token } = useSelector(state => state.reducer);
-    const [myPlaylist,setMyPlaylist]=useState([]);
-    const[messageCode, setMessageCode] = useState(0);
+    const { token } = useSelector(state => state.reducer);
+    const [myPlaylist, setMyPlaylist] = useState([]);
+    const [messageCode, setMessageCode] = useState(0);
 
-    const getPlaylist=async ()=>{
-        try {
-            const response = await axios.create({baseURL: LOCAL_SERVER_URL}).post('/get-playlist?token=' + token);
-            if (response.data.success){
-                setMyPlaylist(response.data.playlist)
-            }else{
-                setMessageCode(response.data.errorCode);
-            }
-            setMessageCode(0);
-
-        }catch (error){
-            console.log(error)
+    const getPlaylist = async () => {
+        const response = await axios.create({ baseURL: LOCAL_SERVER_URL }).post('/get-playlist?token=' + token);
+        if (response.data.success) {
+            setMyPlaylist(response.data.playlist);
+        } else {
+            setMessageCode(response.data.errorCode);
         }
-
+        setMessageCode(0);
     }
 
-    useEffect(()=>{
-        getPlaylist().then(r => {});
-    },[myPlaylist])
-
+    useEffect(() => {
+        getPlaylist();
+    }, [myPlaylist])
 
     return (
-        <View style={globalStyles.flexProp}>
-            <Text>{"\n"}</Text>
-            <Text>My Playlist:</Text>
-            {
-                myPlaylist.length>0?
-                    <View style={globalStyles.flexProp}>
-                        <Player songList={myPlaylist} page={'playlist'} toggleFavorite={null}/>
-                    </View>
-
-                    :
-                    <Text>you haven't had songs</Text>
-
-            }
-            {
-                messageCode!==0&&
-                <ErrorAlert message={messageCode}/>
-            }
-        </View>
+        <ImageBackground source={require('../images/myPlaylist.jpg')} style={myPlaylistStyle.background}>
+            <ScrollView>
+                <View>
+                    {myPlaylist.length > 0 && (
+                        <View style={myPlaylistStyle.textTitle}>
+                            <Text style={myPlaylistStyle.textHeader}>My Playlist:</Text>
+                        </View>
+                    )}
+                    {myPlaylist.length > 0 ? (
+                        <Player songList={myPlaylist} page={'playlist'} toggleFavorite={null} />
+                    ) : (
+                        <View>
+                            <Text style={myPlaylistStyle.noPlaylist}>
+                                You haven't songs
+                            </Text>
+                            <View style={myPlaylistStyle.customButton}>
+                                <TouchableOpacity onPress={() => { navigation.navigate('Popular') }}>
+                                    <Text style={myPlaylistStyle.buttonText}>Go search for more songs</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    )}
+                    {messageCode !== 0 && <ErrorAlert message={messageCode} />}
+                </View>
+            </ScrollView>
+        </ImageBackground>
     );
 }
+
 
--- styles/myPlaylistStyle.js
+++ styles/myPlaylistStyle.js
@@ -2,14 +2,67 @@ import { StyleSheet } from 'react-native';
 
 const myPlaylistStyle = StyleSheet.create({
 
-container: {
-        padding: 10,
-        backgroundColor: 'black',
-},
-text: {
-    fontSize: 16,
-        lineHeight: 24,
-},
+    background: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    },
+
+    button:{
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        height: 50,
+        borderColor: 'black',
+        borderWidth: 5,
+        borderRadius: 20,
+        paddingHorizontal: 10,
+        backgroundColor: 'rgba(255, 255, 255, 0.77)',
+        color: 'black',
+    },
+
+    buttonText: {
+        color: 'black',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+
+    textHeader : {
+        fontSize:42,
+        color: 'white',
+        textShadowColor: 'rgb(227,223,224)',
+        textShadowOffset: { width: 3, height: 3 },
+        textShadowRadius: 4,
+        marginTop:20,
+    },
+
+    textTitle : {
+        alignItems:'center'
+    },
+
+    noPlaylist : {
+        fontSize:40,
+        color: 'white',
+        textShadowColor: 'rgb(255,255,255)',
+        textShadowOffset: { width: 2, height: 2 },
+        textShadowRadius: 4,
+        alignSelf:'center',
+        marginTop:60
+    },
+
+    customButton: {
+        backgroundColor: 'white',
+        borderColor: 'black',
+        borderWidth: 5,
+        borderRadius: 20,
+        paddingHorizontal: 10,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop:550,
+        width: 300,
+        transform: [{ translateX: -50 }, { translateY: -5 }],
+
+    },
 
 });
 export default myPlaylistStyle;
