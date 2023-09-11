6b1b94b58b29df1864def03ce473651700bbbf60
אופיר יהודה <ofir2738@gmail.com>
2023-09-11 22:00:13 +0300

    design myPlaylist.js

--- styles/myPlaylistStyle.js
+++ styles/myPlaylistStyle.js
@@ -1,3 +1,74 @@
+// import { StyleSheet } from 'react-native';
+//
+// const myPlaylistStyle = StyleSheet.create({
+//
+//     background: {
+//         width: '100%',
+//         height: '100%',
+//         resizeMode: 'cover',
+//     },
+//
+//     button:{
+//         flexDirection:'row',
+//         justifyContent: 'space-between',
+//         height: 50,
+//         borderColor: 'black',
+//         borderWidth: 5,
+//         borderRadius: 20,
+//         paddingHorizontal: 10,
+//         backgroundColor: 'rgba(255, 255, 255, 0.77)',
+//         color: 'black',
+//     },
+//
+//     buttonText: {
+//         color: 'black',
+//         fontSize: 16,
+//         fontWeight: 'bold',
+//     },
+//
+//     textHeader : {
+//         fontSize:42,
+//         color: 'white',
+//         textShadowColor: 'rgb(227,223,224)',
+//         textShadowOffset: { width: 3, height: 3 },
+//         textShadowRadius: 4,
+//         marginTop:20,
+//     },
+//
+//     textTitle : {
+//         alignItems:'center'
+//     },
+//
+//     noPlaylist : {
+//         fontSize:40,
+//         color: 'white',
+//         textShadowColor: 'rgb(255,255,255)',
+//         textShadowOffset: { width: 2, height: 2 },
+//         textShadowRadius: 4,
+//         alignSelf:'center',
+//         marginTop:60
+//     },
+//
+//     customButton: {
+//         backgroundColor: 'white',
+//         borderColor: 'black',
+//         borderWidth: 5,
+//         borderRadius: 20,
+//         paddingHorizontal: 10,
+//         height: 50,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         marginTop:440,
+//         // width: 300,
+//         // transform: [{ translateX: -50 }, { translateY: -5 }],
+//
+//     },
+//
+// });
+//
+// export default myPlaylistStyle;
+
+
 import { StyleSheet } from 'react-native';
 
 const myPlaylistStyle = StyleSheet.create({
@@ -40,13 +111,13 @@ const myPlaylistStyle = StyleSheet.create({
     },
 
     noPlaylist : {
-        fontSize:40,
+        fontSize:30,
         color: 'white',
         textShadowColor: 'rgb(255,255,255)',
         textShadowOffset: { width: 2, height: 2 },
         textShadowRadius: 4,
         alignSelf:'center',
-        marginTop:60
+        marginTop:10
     },
 
     customButton: {
@@ -58,12 +129,11 @@ const myPlaylistStyle = StyleSheet.create({
         height: 50,
         justifyContent: 'center',
         alignItems: 'center',
-        marginTop:550,
+        marginTop:0,
         // width: 300,
         // transform: [{ translateX: -50 }, { translateY: -5 }],
 
     },
 
 });
-
 export default myPlaylistStyle;
